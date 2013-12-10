c6dc7d0e1e8a2f88503b84edea2f8463680aeef5
sanderhofman <sanderhofman8@gmail.com>
2013-12-10 16:59:51 +0100

    uploadProject

---
+++ MvcApplication4/MvcApplication4/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MvcApplication4.Models
+{
+    public class Admin
+    {
+        public string password { get; set; }
+       
+
+    }
+}
